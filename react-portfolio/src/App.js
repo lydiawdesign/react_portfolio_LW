0594b85d3d0a77e32ceff8c7185dbae9eb0523c1
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-14 15:35:37 -0500

    adding to form comp

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importing components
 import NavBar from './components/nav';
 import Header from './components/header';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
        < NavBar />
        < Header />
+       < Portfolio />
+       < Contact />
+       < Footer />
     </div>
   );
 }
