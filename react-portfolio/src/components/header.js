3ad969a0b36dacc9e97c5f181b0702a69a77743e
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-14 15:21:22 -0500

    added contact and portfolio components as well as started working on the header for about

--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
+
 export const header = () => {
     return (
         <div className="header-container">
-            <h1>Hi, I'm Lydia! Nice to meet ya!</h1>
+            <div className="main-info">
+                <h1>Hi, I'm Lydia! Nice to meet ya!</h1>
+            </div>
         </div>
-    )
+    );
 }
 
 export default header
