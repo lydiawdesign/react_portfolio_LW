163ec5ceefb510b75148a732afa2141f2c6ba591
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-15 09:14:28 -0500

    refactoring contact form

--- react-portfolio/src/components/contact.js
+++ react-portfolio/src/components/contact.js
@@ -1,50 +1,84 @@
 import React, { useState } from 'react';
 
+// check if the email is valid
+import { validateEmail } from '../utils/helper';
+
 function Form() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
     // Getting the value and name of the input which triggered the change
-    const { name, value } = e.target;
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-    // Ternary statement that will call either setFirstName or setLastName based on what field the user is typing in
-    return name === 'firstName' ? setFirstName(value) : setLastName(value);
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'name') {
+      setName(inputValue);
+    } else {
+        setMessage(inputValue);
+    }
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // Alert the user their first and last name, clear the inputs
-    alert(`Can't wait to hear from you ${firstName}!`);
-    setFirstName('');
-    setLastName('');
+    // check if email is not valid-  If so , an error message will display
+    if (!validateEmail(email) || !name) {
+      setErrorMessage('Please double check that, it looks like your email or name is not correct.');
+      return;
+    }
+  
+    alert(`Thanks for reaching out ${name}`);
+
+    setEmail('');
+    setName('');
+    setMessage('');
   };
 
   return (
     <div>
+      <p>Hello {name}</p>
       <form className="form">
         <input
-          value={firstName}
-          name="firstName"
+          value={email}
+          name="email"
           onChange={handleInputChange}
-          type="text"
-          placeholder="First Name"
+          type="email"
+          placeholder="email"
         />
         <input
-          value={lastName}
-          name="lastName"
+          value={name}
+          name="name"
           onChange={handleInputChange}
           type="text"
-          placeholder="Last Name"
+          placeholder="name"
         />
-        <button type="button" onClick={handleFormSubmit}>
-          Send it!
-        </button>
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>Full Send</button>
       </form>
+
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Form;
+
