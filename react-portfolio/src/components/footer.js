f09295bc688087e4a542542b33675226f0bab1c1
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-17 19:38:27 -0500

    fixed header and footer

--- react-portfolio/src/components/footer.js
+++ react-portfolio/src/components/footer.js
@@ -1,21 +1,20 @@
 import React from "react";  
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+// import '../../src/App.css';
+ 
 function Footer() {
 	return (
-        <footer className="footer">
-            <div>
-                <a href="https://github.com/lydiawdesign" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon="stack-overflow" />
-                </a>
-            </div>
-            <div>
-                <a href="https://www.linkedin.com/in/lydia-williamson/" target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon="stack-overflow" />
-                </a>
-            </div>
         
-        </footer>
+        <div className="footer" id="footer">
+            <div className='img-link'>
+                <a href="https://github.com/lydiawdesign" target="_blank"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon"/></a>
+                <a href="https://www.linkedin.com/in/lydia-williamson/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+            </div>
+            <p className='footer-info'>
+                &copy; Lydia Williamson  |  <a href="https://github.com/lydiawdesign/react_portfolio_LW/blob/main/react-portfolio/src/img/Lydia%20Williamson%20Resume%202021%20September%20Updated.pdf" className="link">download resume</a>
+            </p>
+
+        </div>
+    
 	);
 }
 
