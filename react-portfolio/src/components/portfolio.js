d31a49f59bb7451a9e47ed61670923f53c39a9b0
lydiawdesign <lydia.williamson.OSU@gmail.com>
2021-12-17 20:54:45 -0500

    fixed styling of portfolio page

--- react-portfolio/src/components/portfolio.js
+++ react-portfolio/src/components/portfolio.js
@@ -2,7 +2,7 @@ import React from 'react'
 import '../App.css';
 import groovyMovie from '../img/Groovy Movie Results Screenshot.jpg';
 import passwordGenerator from '../img/password generator screenshot.jpg';
-import teamProfile from '../img/team profile gen post img 1.png';
+import musicReview from '../img/music review.png';
 
 
 export const Portfolio = () => {
@@ -22,28 +22,29 @@ export const Portfolio = () => {
                             </div>
                         </div>
                     </div>
-                </div>
+                
 
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card text-center">
-                        <img src={passwordGenerator} className="card-img-top" alt="password generator"/>
-                        <div className="card-body">
-                            <h5 className="card-title text-uppercase fw-bold">Password Generator</h5>
-                            <p className="card-text">This application enables employees to generate random passwords based on criteria that they’ve selected.</p>
-                            <a href="https://github.com/lydiawdesign/Password_Generator_LW" className="btn btn-outline-dark">See GitHub Project</a>
-                            <a href="/password generator post.html" className="btn btn-outline-dark">Read More</a>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <div className="card text-center">
+                            <img src={passwordGenerator} className="card-img-top" alt="password generator"/>
+                            <div className="card-body">
+                                <h5 className="card-title text-uppercase fw-bold">Password Generator</h5>
+                                <p className="card-text">This application enables employees to generate random passwords based on criteria that they’ve selected.</p>
+                                <a href="https://github.com/lydiawdesign/Password_Generator_LW" className="btn btn-outline-dark">See GitHub Project</a>
+                                <a href="/password generator post.html" className="btn btn-outline-dark">Read More</a>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className="col-12 col-md-6 col-lg-4">
-                    <div className="card text-center">
-                        <img src={teamProfile} className="card-img-top" alt="coming soon"/>
-                        <div className="card-body">
-                            <h5 className="card-title text-uppercase fw-bold">Team Profile Generator</h5>
-                            <p className="card-text">A Node.js command-line app that takes in information about employees, then generates an HTML webpage that displays summaries for each person.</p>
-                            <a href="https://github.com/lydiawdesign/Team_Profile_Generator_LW" className="btn btn-outline-dark">See GitHub Project</a>
-                            <a href="/team profile post.html" className="btn btn-outline-dark">Read More</a>
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card text-center">
+                            <img src={musicReview} class="card-img-top" alt="coming soon"/>
+                            <div class="card-body">
+                                <h5 class="card-title text-uppercase fw-bold">MUSIC REVIEW CLUB</h5>
+                                <p class="card-text">A website that builds a music loving community by allowing users to post song reviews, read music reviews, and comment on reviews once logged in.</p>
+                                <a href="https://github.com/lydiawdesign/music-review-club" class="btn btn-outline-dark">See GitHub Project</a>
+                                <a href="/music review.html" class="btn btn-outline-dark">Read More</a>
+                            </div>
                         </div>
                     </div>
                 </div>
